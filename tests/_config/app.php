8bef8c35e220910aa1fe7343b0e6b3cfe1bccf78
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-09-25 10:35:40 +0000

    Configure env for tests

---
+++ tests/_config/app.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'id' => 'test-app',
+    'class' => \yii\console\Application::class,
+
+    'basePath' => Yii::getAlias('@tests'),
+    'vendorPath' => Yii::getAlias('@vendor'),
+    'runtimePath' => Yii::getAlias('@tests/_output'),
+
+    'bootstrap' => [],
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'sqlite:' . Yii::getAlias('@tests/_output/test.db'),
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+    ],
+    'params' => [],
+];
